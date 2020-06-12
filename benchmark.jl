088158372ce25e888021921f82e1eef254f61e03
Jarosław Królik <krolikjaroslaw@gmail.com>
2020-06-12 18:38:10 +0200

    add BenchmarkGroup

--- benchmark.jl
+++ benchmark.jl
@@ -7,12 +7,15 @@ mutable struct TestedFunction
     TestedFunction(fun, name) = new(fun, name, nothing)
 end
 
+
+# create tested_functions list
 tested_functions = []
 push!(tested_functions, TestedFunction(eigen,"default"))
-push!(tested_functions, TestedFunction(start_power,"power"))
-push!(tested_functions, TestedFunction(start_qr,"qr"))
-push!(tested_functions, TestedFunction(start_house,"house"))
+push!(tested_functions, TestedFunction(eigen_power,"power"))
+push!(tested_functions, TestedFunction(eigen_qr,"qr"))
+push!(tested_functions, TestedFunction(eigen_householder,"house"))
 
+# clear tested_functions
 for i in 1:size(tested_functions)[1]
     pop!(tested_functions)
 end
@@ -35,3 +38,26 @@ function print_bench()
         println(tested_fun.result)
     end
 end
+
+
+t_bench_group = BenchmarkGroup()
+t_functions = (eigen_power, eigen_qr, eigen_householder)
+t_matrices = (rand(3,3), rand(5,5), rand(20,20) )#, rand(50,50), rand(100,100))
+
+for f in t_functions
+    t_bench_group[string(f)] = BenchmarkGroup([f])
+    for A in t_matrices
+        t_bench_group[string(f)][string(size(A))] = @benchmarkable $(f)($A)
+    end
+end
+#t_bench_group[["eigen_power","(3, 3)"]]
+#t_bench_group[[string(t_functions[1]),string(size(t_matrices[1]))]]
+
+# tune!(t_bench_group)
+
+t_results = run(t_bench_group, verbose = true, seconds = 1)
+
+
+# t_results[string(t_functions[1])][string(size(t_matrices[1]))]
+# t_results[[string(t_functions[1]), string(size(t_matrices[1]))]]
+median(t_results[string(t_functions[1])][string(size(t_matrices[1]))])
