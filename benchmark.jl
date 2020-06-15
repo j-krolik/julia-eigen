0ede1e15aafe6d232c0a3ac2303502549b814416
Jarosław Królik <krolikjaroslaw@gmail.com>
2020-06-15 14:05:27 +0200

    Zip

--- benchmark.jl
+++ benchmark.jl
@@ -1,129 +1,71 @@
 using BenchmarkTools
 using Plots
 
-mutable struct TestedFunction
-    fun::Function
-    name::String
-    result
-    TestedFunction(fun, name) = new(fun, name, nothing)
+bench_group = BenchmarkGroup()
+iteration_no = 21
+functions = [eigen_power, eigen_qr, eigen_householder]
+matrices = (3,3),(5,5),(10,10),(20,20),(40,40),(60,60),(80,80),(100,100)
+matrices_string = Vector(undef, size(matrices,1))
+for matix_no = 1:size(matrices,1)
+    matrices_string[matix_no] = string(matrices[matix_no])
 end
 
-
-# create tested_functions list
-tested_functions = []
-push!(tested_functions, TestedFunction(eigen,"default"))
-push!(tested_functions, TestedFunction(eigen_power,"power"))
-push!(tested_functions, TestedFunction(eigen_qr,"qr"))
-push!(tested_functions, TestedFunction(eigen_householder,"house"))
-
-# clear tested_functions
-for i in 1:size(tested_functions)[1]
-    pop!(tested_functions)
+# generowanie macierzy, które będą podawane benchmarkowi
+A = Array{Any,2}(undef, iteration_no, size(matrices,1))
+for i = 1:iteration_no, A_no = 1:size(matrices,1)
+    A[i,A_no] = rand(matrices[A_no][1],matrices[A_no][2])
 end
 
-foo = [12 -51 4; 6 167 -68; -4 24 -41] * 1.0
-
-function bench(a=foo)
-    global fun
-    for tested_fun in tested_functions
-        fun = tested_fun.fun
-        b = @benchmarkable (fun($a)) seconds=1.0
-        tune!(b)
-        tested_fun.result = (run(b))
-    end
-    print_bench()
-end
-
-function print_bench()
-    for tested_fun in tested_functions
-        println(tested_fun.result)
-    end
-end
-
-##################
-##################
-
-
-t_bench_group = BenchmarkGroup()
-t_functions = (eigen_power, eigen_qr, eigen_householder)
-#t_functions = (eigen_power, eigen_householder)
-t_matrices = (3,3), (5,5) #, (20,20), (50,50), (100,100))
-t_iteration_no = 3
-
-t_functions_string = Vector{String}(undef, size(t_functions,1))
-for f_no = 1:size(t_functions,1)
-    t_functions_string[f_no] = string(t_functions[f_no])
-end
-t_matrices_string = Vector(undef, size(t_matrices,1))
-for matix_no = 1:size(t_matrices,1)
-    t_matrices_string[matix_no] = string(size(t_matrices[matix_no],1))
-end
-
-
-t_A = Array{Any,2}(undef, t_iteration_no, size(t_matrices,1))
-for i = 1:t_iteration_no, A_no = 1:size(t_matrices,1)
-    t_A[i,A_no] = rand(t_matrices[A_no][1],t_matrices[A_no][2])
-end
-
-
-for f in t_functions
-    t_bench_group[string(f)] = BenchmarkGroup([f])
-    for A_no = 1:size(t_matrices,1)
-        t_bench_group[string(f), string(size(A))] = BenchmarkGroup([f,A])
-        for i = 1:t_iteration_no
-            A = t_A[i,A_no]
-            t_bench_group[string(f), string(size(A)), i] = @benchmarkable $(f)($A)
+# definiowanie procesu benchmarku
+for f in functions
+    bench_group[string(f)] = BenchmarkGroup([f])
+    for A_no = 1:size(matrices,1)
+        bench_group[string(f), string(size(A))] = BenchmarkGroup([f,A])
+        for i = 1:iteration_no
+            A = A[i,A_no]
+            bench_group[string(f), string(size(A)), i] = @benchmarkable $(f)($A)
         end
     end
 end
 
-#t_bench_group[["eigen_power","(3, 3)"]]
-#t_bench_group[[string(t_functions[1]),string(size(t_matrices[1]))]]
+# uruchomienie benchmarka
+results_raw = run(bench_group, verbose = true, seconds = 1)
 
-# tune!(t_bench_group)
-t_results_raw = run(t_bench_group, verbose = true, seconds = 1)
-
-# t_results[string(t_functions[1])][string(size(t_matrices[1]))]
-# t_results[[string(t_functions[1]), string(size(t_matrices[1]))]]
-# median(t_results_raw[string(t_functions[1])][string(size(t_matrices[1]))])
-
-t_result_time = Matrix{Float64}(undef,size(t_matrices,1),size(t_functions,1))
-t_result_memory = Matrix{Int64}(undef,size(t_matrices,1),size(t_functions,1))
-for f_no = 1:size(t_functions,1)
-    for A_no = 1:size(t_matrices,1)
-        time_sum   = 0
-        memory_sum = 0
-        for i = 1:t_iteration_no
-            median_result = median(t_results_raw[string(t_functions[f_no])][string(size(t_matrices[A_no]))])
-            time_sum   += median_result.time
-            memory_sum += median_result.memory
+# ekstracja danych z postaci surowej
+resultime = Matrix{Float64}(undef,size(matrices,1),size(functions,1)+1)
+resulmemory = Matrix{Float64}(undef,size(matrices,1),size(functions,1)+1)
+for f_no = 1:size(functions,1)
+    for A_no = 1:size(matrices,1)
+        time_sum   = Float64[]
+        memory_sum = Float64[]
+        for i = 1:iteration_no
+            if f_no < 4
+                median_result = median(results_raw[string(functions[f_no]), string(matrices[A_no]), i])
+            else
+                median_result = median(results_raw_small[string(functions[f_no-3]), string(matrices[A_no]), i])
+            end
+            push!(time_sum, median_result.time)
+            push!(memory_sum, median_result.memory)
         end
-        t_result_time[A_no,f_no]   = time_sum/t_iteration_no
-        t_result_memory[A_no,f_no] = memory_sum/t_iteration_no
+        resultime[A_no,f_no]   = sum(time_sum)/iteration_no
+        resulmemory[A_no,f_no] = sum(memory_sum)/iteration_no
     end
 end
 
-plot(t_matrices_string, t_result_time/1000,
-#plot(t_matrices_string, t_result_time[:,[1,3]],
+plot(matrices_string, resultime/1000,
     yaxis = (:log10),
     title = "Mediana czasu w zależności wyznacznia wektrów od rozmiaru macierzy",
     titlefontsize = 10,
     ylabel = "Czas, ms",
     xlabel = "Rozmar macierzy",
     legend = :topleft,
-    label = ["Power" "QR" "Householder"])
-    #label = ["Power" "Householder"])
+    label = ["Power Δ=10^-7" "QR Δ=10^-7" "Householder Δ=10^-7" "Power Δ=10^-6"])
 
-plot(t_matrices_string, t_result_memory/1024,
-#plot(t_matrices_string, t_result_time[:,[1,3]],
+plot(matrices_string, resulmemory/1024,
     yaxis = (:log10),
     title = "Użycie pamięci w zależności od rozmiaru macierzy",
     titlefontsize = 10,
     ylabel = "Użycie pamięci, KiB",
     xlabel = "Rozmar macierzy",
     legend = :topleft,
-    label = ["Power" "QR" "Householder"])
-    #label = ["Power" "Householder"])
-
-
-plot(["a","c","b"], 1:3)
+    label = ["Power Δ=10^-7" "QR Δ=10^-7" "Householder Δ=10^-7" "Power Δ=10^-6"])
--- eigen_algorithms.jl
+++ eigen_algorithms.jl
@@ -19,7 +19,7 @@ function power_metod(A::Matrix{T}, print_it=false, iteration=nothing, delta=5*10
      @inbounds for k in 1:iteration
         eigen_vector = A*eigen_vector./eigen_value
         eigen_value = norm(eigen_vector)
-        # chech if enought
+        # if enought delta enought small, break
         if abs(last_value-eigen_value)<delta print_it&&k!=1&&println(k); break end
         last_value=eigen_value
     end
@@ -31,7 +31,6 @@ function eigen_qr(A::Matrix, print_it=false)
     if (typeof(A)!=Matrix{Float64})
         A=convert(Matrix{Float64},A)
     end
-   #return E_qr=qr_q_eigen(A)
    return qr_q_eigen(A, print_it)
 end
 
@@ -46,7 +45,7 @@ function qr_q_eigen(A::Matrix{T}, print_iteration=false, delta=5*10^-6) where {T
     Q = Matrix{T}(undef,dim,dim)
     last_value::T = last(A)
     for k in 1:10000
-        # A-(QR)->Q
+        # QR factorization
         @inbounds for col in 1:dim
            Q[:,col] =(A[:,col])
            @inbounds for col_proj in 1:(col-1)
@@ -58,7 +57,7 @@ function qr_q_eigen(A::Matrix{T}, print_iteration=false, delta=5*10^-6) where {T
         end
         # calc eigen
         A = Q'*A*Q
-        # chech if enought
+        # if enought delta enought small, break
         if abs(last(A)-last_value)<delta print_iteration&&println(k); break end
         last_value=last(A)
 
@@ -104,6 +103,7 @@ function qr_householder_eigen!(A::Matrix{T}, print_iteration::Bool=false, delta=
     last_value::T = last(A)
     Q = Array{T,2}(I,dim,dim)
     for k in 1:100
+        # Householder factorization
         for k in 1:dim
             z = A[k:dim, k]
             v = [ -sign(z[1])*norm(z) - z[1]; -z[2:end] ]
@@ -111,7 +111,7 @@ function qr_householder_eigen!(A::Matrix{T}, print_iteration::Bool=false, delta=
             Q[k:dim,:] = Q[k:dim,:] - v*( 2*(v'*Q[k:dim,:]) )
         end
         A = Q*A*Q'
-        # if delta enought small, brek form loop
+        # if enought delta enought small, break
         if abs(last(A)-last_value)<delta print_iteration&&println(k); break end
         last_value=last(A)
         Q = Array{Float64,2}(I,dim,dim)
